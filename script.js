47c54094a483de7c6ba5fe885411b089c3f0c7dd
AABID DEVELOPER <150765769+Aabid404@users.noreply.github.com>
2025-08-30 22:27:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+// Get references
+const select = document.getElementById("colorSelect");
+const button = document.querySelector('input[type="button"]');
+
+// Add event listener to button
+button.addEventListener("click", function () {
+  // Find the selected option index
+  const selectedIndex = select.selectedIndex;
+
+  // If an option is selected, remove it
+  if (selectedIndex !== -1) {
+    select.remove(selectedIndex);
+  }
+});
+ 
